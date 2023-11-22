3e6dfb8b20b0da7557f1c7110f4ea7d653a25c88
tharhtetaung <tharhtetaung@my.jcu.edu.au>
2023-11-22 18:35:07 +0800

    Refactor password check program to use functions

---
+++ prac_02/score.py
@@ -0,0 +1,28 @@
+
+import random
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+
+
+def main():
+    score = int(input("Enter score: "))
+    result = determine_result(score)
+    random_score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
+    print(f"Random score is {random_score}")
+    random_result = determine_result(random_score)
+    print(result)
+    print(random_result)
+
+
+def determine_result(score):
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
